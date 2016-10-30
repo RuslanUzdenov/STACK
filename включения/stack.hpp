141a822b714f3f4cb6442f093e157febb1cf0f01
RuslanUzdenov <0508rus@bk.ru>
2016-10-30 23:50:32 +0300

    Update and rename include/stack.hpp to включения/stack.hpp

--- включения/stack.hpp
+++ включения/stack.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <stdexcept>
 
 using namespace std;
 
@@ -12,36 +11,27 @@ size_t max(size_t a, size_t b) {
     return a > b ? a : b;
 }
 
-template <typename T>
-T* new_with_copy(const T *tmp, size_t count, size_t array_size) {  /* strong */
-    T *array_ = new T[array_size];
-    copy(tmp, tmp + count, array_);
-    return array_;
-}
-
 template<typename T>
 class Stack {
 public:
-    Stack(); /* noexcept*/
+    Stack();
 
-    ~Stack(); /* noexcept */
+    ~Stack();
 
-    Stack(const Stack &); /* strong */
+    Stack(const Stack &);
 
-    Stack& operator = (const Stack &); /* strong */
+    Stack& operator = (const Stack &);
 
-    size_t count() const; /* noexcept */
+    size_t count() const;
 
-    void push(T const &); /* strong */
+    void push(T const &);
 
-    void pop();  /* strong */
+    T pop();
 
-    T top() const; /* noexcept */
-
-    bool is_empty() const; /* noexcept */
+    bool is_empty() const;
 
 private:
-    void grow(); /* strong */
+    void grow();
 
     T *array_;
     size_t array_size_;
@@ -76,7 +66,8 @@ void Stack<T>::push(T const &element) {
 template<typename T>
 void Stack<T>::grow() {
     size_t new_array_size_ = max(1, array_size_ * 2);
-    T *new_array_ = new_with_copy(array_, count_, new_array_size_);
+    T *new_array_ = new T[new_array_size_];
+    copy(array_, array_ + count_, new_array_);
     if (!is_empty()) {
         delete[] array_;
     }
@@ -86,24 +77,23 @@ void Stack<T>::grow() {
 }
 
 template<typename T>
-void Stack<T>::pop() {
+T Stack<T>::pop() {
     if (is_empty()) {
         throw std::logic_error("Stack is empty!");
     }
-    --count;
-}
-
-template <typename T>
-T Stack<T>::top() const {
-    if (is_empty()) {
-        throw std::logic_error("Stack is empty!");
-    }
-    return array_[count_ - 1];
+    return array_[--count_];
 }
 
 template<typename T>
 bool Stack<T>::is_empty() const {
     return count_ == 0;
+}
+
+template <typename T>
+T* new_with_copy(const T *tmp, size_t count, size_t array_size) {
+    T *array_ = new T[array_size];
+    copy(tmp, tmp + count, array_);
+    return array_;
 }
 
 template <typename T>
@@ -116,14 +106,10 @@ Stack<T>::Stack(const Stack &tmp)
 
 template <typename T>
 Stack<T>& Stack<T>::operator=(const Stack<T> &tmp) {
-    if (this == &tmp) {
-    }   else {
-        count_ = tmp.count_;
-        array_size_ = tmp.array_size_;
-        delete[] array_;
-        array_ = new_with_copy(tmp.array_, count_, array_size_);
-
-    }
+    if (this == &tmp) {}
+    count_ = tmp.count_;
+    array_size_ = tmp.array_size_;
+    array_ = new_with_copy(tmp.array_, count_, array_size_);
     return *this;
 }
 
